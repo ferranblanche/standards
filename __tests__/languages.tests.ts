5e820d29d6e6794da9bc6d74e382a9166fbc0b5d
Ferran Blanché <ferran.blanche@gmail.com>
2022-08-24 19:10:27 +0200

    Enhanced test case - should return true when validating a 2-letter ISO code, and false otherwise

--- __tests__/languages.tests.ts
+++ __tests__/languages.tests.ts
@@ -2,12 +2,16 @@ import { Languages } from "../src";
 
 describe('Language Service', () => {
     let languages = new Languages;
-    const testCases = [
-        { code: 'es', valid: true },
-        { code: 'en', valid: true },
-        { code: 'spanish', valid: false }
-    ];
-    test.each(testCases)('should return $valid when validating $code is a 2-letter ISO code', (testCase) => {
-        expect(languages.isValid(testCase.code)).toBe(testCase.valid);
-    });
+    describe('Validate 2-letter ISO code', () => {
+        const testCases = [
+            { code: 'es', valid: true },
+            { code: 'en', valid: true },
+            { code: 'xx', valid: false },
+            { code: 'spanish', valid: false }
+        ];
+        test.each(testCases)('should return $valid when validating $code is a 2-letter ISO code', (testCase) => {
+            expect(languages.isValid(testCase.code)).toBe(testCase.valid);
+        });
+    }
+    )
 });
