9f7ac454b2c1095b48867b53f0ec177e0efae2ce
Ferran Blanché <ferran.blanche@gmail.com>
2022-09-19 22:31:25 +0200

    test(Languages): :test_tube: Add tests to search Language by Text

--- __tests__/languages.ts
+++ __tests__/languages.ts
@@ -7,9 +7,9 @@ describe('Language Service', () => {
             { code: 'es', valid: true },
             { code: 'en', valid: true },
             { code: 'xx', valid: false },
-            { code: 'spanish', valid: false }
+            { code: 'spanish', valid: false },
         ];
-        test.each(testCases)('should return $valid when validating $code is a 2-letter ISO code', (testCase) => {
+        test.each(testCases)('should return "$valid" when validating "$code" as a 2-letter ISO code', (testCase) => {
             expect(languages.contains(testCase.code)).toBe(testCase.valid);
         });
     }
@@ -19,10 +19,21 @@ describe('Language Service', () => {
             { code: 'es', result: { "code": "es", "name": "Spanish", "local": "Español" } },
             { code: 'ja', result: { "code": "ja", "name": "Japanese", "local": "日本語" } },
             { code: 'xx', result: undefined },
-            { code: 'spanish', result: undefined }
+            { code: 'spanish', result: undefined },
         ];
-        test.each(testCases)('should return $result when validating $code is a 2-letter ISO code', (testCase) => {
+        test.each(testCases)('should return "$result" when getting "$code" information', (testCase) => {
             expect(languages.get(testCase.code)).toEqual(testCase.result);
         })
-    })
+    });
+    describe('Search Language by text', () => {
+        const testCases = [
+            { criterion: 'nl', result: { "code": "nl", "name": "Dutch", "local": "Nederlands" } },
+            { criterion: 'Span', result: { "code": "es", "name": "Spanish", "local": "Español" } },
+            { criterion: '語', result: { "code": "ja", "name": "Japanese", "local": "日本語" } },
+            { criterion: 'Span本語', result: undefined },
+        ];
+        test.each(testCases)('should return "$result" when searching by "$criterion"', (testCase) => {
+            expect(languages.search(testCase.criterion)).toEqual(testCase.result);
+        })
+    });
 });
