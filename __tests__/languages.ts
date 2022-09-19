7ebab769aabad12719830a09fbc00ebee613194d
Ferran Blanché <ferran.blanche@gmail.com>
2022-09-19 20:52:08 +0200

    test: :test_tube: Add tests for getting a Language by code

--- __tests__/languages.ts
+++ __tests__/languages.ts
@@ -13,5 +13,16 @@ describe('Language Service', () => {
             expect(languages.contains(testCase.code)).toBe(testCase.valid);
         });
     }
-    )
+    );
+    describe('Get Language by 2-letter ISO code', () => {
+        const testCases = [
+            { code: 'es', result: { "code": "es", "name": "Spanish", "local": "Español" } },
+            { code: 'ja', result: { "code": "ja", "name": "Japanese", "local": "日本語" } },
+            { code: 'xx', result: undefined },
+            { code: 'spanish', result: undefined }
+        ];
+        test.each(testCases)('should return $result when validating $code is a 2-letter ISO code', (testCase) => {
+            expect(languages.get(testCase.code)).toEqual(testCase.result);
+        })
+    })
 });
