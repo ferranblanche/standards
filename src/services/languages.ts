d0f23d18e5b40c0e65b3f2a3bd7fa61eca799bc0
Ferran Blanché <ferran.blanche@gmail.com>
2022-09-20 15:31:41 +0200

    refactor(Languages): :truck: Unify types and interfaces in a single file

--- src/services/languages.ts
+++ src/services/languages.ts
@@ -1,9 +1,8 @@
 // const languagesData = require('./languages.data.json');
 import { languages } from '../data';
-import { Dictonary } from '../interfaces';
-import { Language } from '../types';
+import { Language, LanguageDictonary } from '../types';
 
-export class LanguageService implements Dictonary {
+export class LanguageService implements LanguageDictonary {
     public contains(code: string): boolean {
         return languages.find(language => language.code === code) ? true : false;
     };
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,19 @@
-export type Language = {
+type Term = {
     code: string,
     name: string,
     local: string
+}
+
+export interface Dictonary {
+    contains(code: string): boolean
+    get(code: string): Term | undefined
+    search(criterion: string): Term | undefined
+}
+
+export type Language = Term
+
+export interface LanguageDictonary extends Dictonary {
+    contains(code: string): boolean
+    get(code: string): Language | undefined
+    search(criterion: string): Language | undefined
 }
